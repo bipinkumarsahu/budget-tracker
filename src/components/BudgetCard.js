a8794dce9720867a95d85723c6b442b9aab54041
Bipin Sahu <bipinsahu5@gmail.com>
2022-07-27 22:59:54 +0530

    Made Card Component

---
+++ src/components/BudgetCard.js
@@ -0,0 +1,15 @@
+import Card from 'react-bootstrap/Card';
+function BudgetCard({name, amount, max}) {
+  return (
+   <Card>
+    <Card.Body>
+        <Card.Title>
+            <div>{name}</div>
+            <div>{amount} / {max}</div>
+        </Card.Title>
+    </Card.Body>
+   </Card>
+  )
+}
+
+export default BudgetCard
